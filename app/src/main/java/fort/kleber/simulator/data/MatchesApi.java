3ebe83b0c507dd953aebc528749a2ad908857dc2
kleberfort <kleberfort@gmail.com>
2022-02-10 11:51:43 -0300

    Finalização do App de simulação de partidas

---
+++ app/src/main/java/fort/kleber/simulator/data/MatchesApi.java
@@ -0,0 +1,13 @@
+package fort.kleber.simulator.data;
+
+import java.util.List;
+
+import fort.kleber.simulator.domain.Match;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MatchesApi {
+    @GET("matches.json")
+    Call<List<Match>> getMatches();
+
+}
